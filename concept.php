6bb590fd16ccbd8558fc9be64312bbe1812f48c9
Suzoka/Morgan <mor9an77@gmail.com>
2023-03-30 15:49:24 +0200

    :lipstick: change font

--- concept.php
+++ concept.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Le concept</title>
     <link rel="stylesheet" href="style.css">
+    <link rel="icon" href="./photos/favicon.svg" type="image/svg+xml">
 </head>
 
 <body>
@@ -24,29 +25,9 @@
     <h1 class="top titre" id="top">Notre concept</h1>
 
     <p>
-        Test Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque hic pariatur autem odio porro,
-        dolor nemo, sequi amet accusamus aliquid eius tempora dignissimos, fugiat aliquam sunt nulla! Eos, enim
-        sit!Lorem
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Blanditiis repudiandae reprehenderit inventore aliquid
-        assumenda, soluta molestiae fuga in, quis eaque quas rerum suscipit, omnis minus cum! Minus quos excepturi
-        aspernatur.
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat amet rem ab ipsum odio labore magnam,
-        doloremque iste dolor obcaecati? Tenetur voluptate quidem consectetur quo at blanditiis beatae impedit ex.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea necessitatibus sed repellat suscipit nulla molestias
-        enim voluptate illo, ipsa cumque libero odit aperiam tenetur quisquam aut repudiandae a veniam voluptas.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit, voluptatem? Esse, architecto libero harum,
-        quisquam voluptatibus odit dolorum cumque voluptatum corporis tenetur beatae maxime praesentium? Autem cumque nulla
-        inventore natus?
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, nostrum? Sed officia ex quaerat natus reprehenderit
-        consequuntur necessitatibus laudantium assumenda iusto velit veritatis, eveniet suscipit nam dolor error, saepe
-        minus.
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate repellendus voluptas odit fuga temporibus
-        nostrum? Similique exercitationem quidem illum rem, dolorum qui quisquam nisi ex sapiente deserunt quam delectus
-        beatae.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt voluptas esse hic quasi commodi dicta id dolores
-        molestias corrupti sapiente unde aut, rem ad. Ratione suscipit sed dicta blanditiis architecto?
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. In sint accusamus exercitationem placeat quas voluptatum,
-        pariatur velit, magni ipsum a voluptatem quibusdam cumque numquam excepturi illum veniam, possimus qui dolorem!
+        Fructus et legumina est une initiative de la ceuillette aux fleurs. <br>Le principe est simple : vous n'avez pas
+        le temps de venir ceuillir nos fruits bio ? Alors reservez votre pannier tout fait ! <br><br>Toutes les
+        semaines, venez chercher votre pannier remplis de fruits Bio et de saisons issus de notre ceuillette.
     </p>
 
     <script src="./script/nav.js"></script>
