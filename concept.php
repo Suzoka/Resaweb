51648d02351459e4bf2d41998cf9335761a12491
Suzoka/Morgan <mor9an77@gmail.com>
2023-03-30 10:47:51 +0200

    :construction: patch some stuff and add favicon

--- concept.php
+++ concept.php
@@ -12,7 +12,7 @@
 <body>
     <header>
         <a href="#navigation" class="skip-link">Aller à la barre de navigation</a>
-        <a href="#contenu" class="skip-link">Aller au contenu</a>
+        <a href="#top" class="skip-link">Aller au contenu</a>
         <nav id="navigation">
             <a href="./index.php" class="logo"><img src="./photos/logo.svg" alt="Accueil"></a>
             <a href="./concept.php">Le concept</a>
@@ -21,7 +21,10 @@
         </nav>
     </header>
 
-    <h1 class="toto" id="top">Test Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque hic pariatur autem odio porro,
+    <h1 class="top titre" id="top">Notre concept</h1>
+
+    <p>
+        Test Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque hic pariatur autem odio porro,
         dolor nemo, sequi amet accusamus aliquid eius tempora dignissimos, fugiat aliquam sunt nulla! Eos, enim
         sit!Lorem
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Blanditiis repudiandae reprehenderit inventore aliquid
@@ -29,13 +32,23 @@
         aspernatur.
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat amet rem ab ipsum odio labore magnam,
         doloremque iste dolor obcaecati? Tenetur voluptate quidem consectetur quo at blanditiis beatae impedit ex.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea necessitatibus sed repellat suscipit nulla molestias enim voluptate illo, ipsa cumque libero odit aperiam tenetur quisquam aut repudiandae a veniam voluptas.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit, voluptatem? Esse, architecto libero harum, quisquam voluptatibus odit dolorum cumque voluptatum corporis tenetur beatae maxime praesentium? Autem cumque nulla inventore natus?
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, nostrum? Sed officia ex quaerat natus reprehenderit consequuntur necessitatibus laudantium assumenda iusto velit veritatis, eveniet suscipit nam dolor error, saepe minus.
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate repellendus voluptas odit fuga temporibus nostrum? Similique exercitationem quidem illum rem, dolorum qui quisquam nisi ex sapiente deserunt quam delectus beatae.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt voluptas esse hic quasi commodi dicta id dolores molestias corrupti sapiente unde aut, rem ad. Ratione suscipit sed dicta blanditiis architecto?
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. In sint accusamus exercitationem placeat quas voluptatum, pariatur velit, magni ipsum a voluptatem quibusdam cumque numquam excepturi illum veniam, possimus qui dolorem!
-    </h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea necessitatibus sed repellat suscipit nulla molestias
+        enim voluptate illo, ipsa cumque libero odit aperiam tenetur quisquam aut repudiandae a veniam voluptas.
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit, voluptatem? Esse, architecto libero harum,
+        quisquam voluptatibus odit dolorum cumque voluptatum corporis tenetur beatae maxime praesentium? Autem cumque nulla
+        inventore natus?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, nostrum? Sed officia ex quaerat natus reprehenderit
+        consequuntur necessitatibus laudantium assumenda iusto velit veritatis, eveniet suscipit nam dolor error, saepe
+        minus.
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate repellendus voluptas odit fuga temporibus
+        nostrum? Similique exercitationem quidem illum rem, dolorum qui quisquam nisi ex sapiente deserunt quam delectus
+        beatae.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt voluptas esse hic quasi commodi dicta id dolores
+        molestias corrupti sapiente unde aut, rem ad. Ratione suscipit sed dicta blanditiis architecto?
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. In sint accusamus exercitationem placeat quas voluptatum,
+        pariatur velit, magni ipsum a voluptatem quibusdam cumque numquam excepturi illum veniam, possimus qui dolorem!
+    </p>
+
     <script src="./script/nav.js"></script>
 </body>
 
