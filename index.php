c6ae37c1fbf20310db1ad1b0bd7b92e71fc682cb
Suzoka/Morgan <mor9an77@gmail.com>
2023-05-23 17:00:19 +0200

    :lipstick:

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Resaweb</title>
+    <title>Fructus & legumina - Accueil</title>
     <link rel="stylesheet" href="style.css">
     <link rel="icon" href="./photos/favicon.svg" type="image/svg+xml">
 </head>
